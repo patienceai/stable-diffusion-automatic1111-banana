f32446887ec088633c5137b402193c5ba1f8bf70
patienceai <121554689+patienceai@users.noreply.github.com>
2023-02-05 01:57:27 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import torch
 import modules.safe as safe
 import modules.shared as shared
+import modules.sd_hijack as sd_hijack
 import modules.sd_models
 from modules.timer import Timer
+from modules import sd_hijack
 
 torch.load = safe.unsafe_torch_load
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -10,20 +12,16 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 def noop(*args, **kwargs):
     pass
 
-def get_checkpoint_info():
-    return list(modules.sd_models.checkpoints_list.values())[0]
-
-def load_weights():
-    checkpoint = get_checkpoint_info()
-    return modules.sd_models.get_checkpoint_state_dict(checkpoint, Timer())
+def load_model():
+    modules.sd_models.load_model()
+    return shared.sd_model
 
 def init():
     global model
+    shared.cmd_opts.no_hashing = True
     modules.sd_models.list_models()
     modules.sd_models.list_models = noop
-    model = load_weights()
-
-def apply_weights():
-    global model
-    checkpoint = get_checkpoint_info()
-    modules.sd_models.checkpoints_loaded[checkpoint] = model
+    model = load_model()
+    modules.sd_models.load_model = noop
+    sd_hijack.model_hijack.hijack(model)
+    shared.sd_model = model
--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import webui
 import app as user_src
 
 user_src.init()
-user_src.apply_weights()
 
 if __name__ == '__main__':
     webui.api_only()
